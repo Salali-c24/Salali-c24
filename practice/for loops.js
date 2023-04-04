35ae69cda3db4c55eaa9945f05fb69430ac1a276
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-04-04 15:59:14 -0400

    practice - js for loops

---
+++ practice/for loops.js
@@ -0,0 +1,18 @@
+// 1-10 IS EVEN 1-10 IS ODD
+
+// for (let i = 0; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i + " is even");
+//     } else {
+//         console.log(i + " is odd");
+//     }
+// }
+
+// LIST ONLY EVENS
+
+console.log("Even numbers up to 10:");
+for (let i = 1; i < 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
