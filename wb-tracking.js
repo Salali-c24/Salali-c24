1809880023805cb6fa8c5367474e17e421a083f2
thecowsays <117232523+thecowsays@users.noreply.github.com>
2023-04-09 21:00:57 -0400

    added JS & boilerplate for practice project

---
+++ wb-tracking.js
@@ -0,0 +1,18 @@
+// CHARACTER NAMES WITH BUFFS
+const charName = ["Player1", "Player2", "Player3", "Player4", "Player5"];
+// PLAYER WHO OWNS THE CHARACTER(S)
+const charOwner = ["username1", "username2", "username3", "username4", "username5"];
+
+// DS CONSISTS OF TWO VALUES, EITHER WILL MAKE TRUE,
+// BUT MUST KNOW WHICH VALUE IS WHICH IN A 'BREAKDOWN'
+// I.E. DS = A || B FOR TRUE, BUT VALUE OF A OR B MUST BE
+// SAVED FOR REFERENCE LATER ON...
+const ds = [0, 1, 2];
+// THESE TWO MUST BE TRUE OR FALSE ONLY
+const zg = "1";
+const wcb = "1";
+// MAYBE THIS SHOULD BE AN OBJECT? IT WILL EVENTUALLY BE AN
+//ARRAY OF KEY-VALUE PAIRS
+const buffs = [zg, ds, wcb];
+
+console.log(buffs);
